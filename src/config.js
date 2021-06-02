f8b4c849a7151eda209b81d7fa4f6339c45a18c9
Ricardo Medina <rmedina0531@gmail.com>
2021-06-02 16:28:56 -0700

    added requirements for cognito log in

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = {
+  apiGateway: {
+    REGION: "YOUR_API_GATEWAY_REGION",
+    URL: "YOUR_API_GATEWAY_URL",
+  },
+  cognito: {
+    REGION: "us-west-1",
+    USER_POOL_ID: "us-west-1_K4UjXwqMX",
+    APP_CLIENT_ID: "3fdaquj0r5binauo15q815o2ai",
+    IDENTITY_POOL_ID: "us-west-1:d9e0492a-aab8-451b-ae41-9dc74d1fbd1f",
+  },
+};
+
+export default config;
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 // import Button from "react-bootstrap/Button";
 
-import Button from 'react-bootstrap/Button'
+import { Auth } from "aws-amplify";
+
+import Button from "react-bootstrap/Button";
 import "./Login.css";
 
 export default function Login() {
@@ -13,20 +15,30 @@ export default function Login() {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  // }
+
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      alert("Logged in");
+    } catch (e) {
+      alert.apply(e.message);
+    }
   }
 
   return (
     <div className="Login">
       <Form onSubmit={handleSubmit}>
-
         <Form.Group size="lg" controlId="email">
-          <Form.Label className='logLabel'>Email : </Form.Label>
+          <Form.Label className="logLabel">Email : </Form.Label>
 
           <Form.Control
             autoFocus
-            className='txtField'
+            className="txtField"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -34,20 +46,19 @@ export default function Login() {
         </Form.Group>
 
         <Form.Group size="lg" controlId="password">
-          <Form.Label className='logLabel'>Password : </Form.Label>
+          <Form.Label className="logLabel">Password : </Form.Label>
 
           <Form.Control
-            className='txtField'
+            className="txtField"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
 
-        <Button className='lgbtn' type="submit" disabled={!validateForm()}>
+        <Button className="lgbtn" type="submit" disabled={!validateForm()}>
           Login
         </Button>
-
       </Form>
     </div>
   );
